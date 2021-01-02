54c4cc174e889f5ec7ee6b0be140ef2c599ec355
Rik Brown <rik@rik.codes>
2021-01-02 14:39:54 -0800

    Add ESO background

--- src/app/modules/app/preloader/preloader.component.ts
+++ src/app/modules/app/preloader/preloader.component.ts
@@ -7,7 +7,7 @@ import {AVAILABLE_COIN_STYLES} from "../../../coin/constants";
   styleUrls: ['./preloader.component.scss']
 })
 export class PreloaderComponent implements OnInit {
-  ROUND_BACKGROUNDS = ['EU4', 'Civ', 'Stellaris', 'HoI', 'WoW', 'SoT']
+  ROUND_BACKGROUNDS = ['EU4', 'Civ', 'Stellaris', 'HoI', 'WoW', 'SoT', 'ESO']
   COIN_ASSETS = AVAILABLE_COIN_STYLES.flatMap(c => [
     'assets/img/coins/' + c + '/heads.png',
     'assets/img/coins/' + c + '/tails.png',
